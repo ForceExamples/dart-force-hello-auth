76291b598bf4bcb66daacbc1f0c24e24893c0958
joris hermans <JHermans85@hotmail.com>
2014-05-16 12:52:40 +0200

    example app with authentication showcase

---
+++ web/index.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'dart:html';
+
+import 'package:force/force_browser.dart';
+
+void main() {
+  ForceClient forceClient = new ForceClient(usePolling: true);
+  forceClient.connect();
+  
+  int count = 0;
+  
+  AnchorElement element = querySelector("#foo");
+  element.onClick.listen((e) {
+    count++;
+    forceClient.send('count', {"count" : count, "foo" : "hello"} );
+  });
+  
+  AnchorElement secElement = querySelector("#secure");
+  secElement.onClick.listen((e) {
+      count++;
+      forceClient.send('secure', {"count" : count, "secure" : "hello"} );
+    });
+  
+  AnchorElement loginElement = querySelector("#login");
+  loginElement.onClick.listen((e) {
+       count++;
+       var profileInfo = { 'name' : 'login'};
+       forceClient.initProfileInfo(profileInfo);
+     });
+  
+  
+  
+  forceClient.on("notifications", (e, sender) {
+    var json = e.json;
+    print("received $json");
+  });
+  
+  forceClient.on("unauthorized", (e, sender) {
+      var json = e.json;
+      print("unauthorized $json");
+    });
+}
